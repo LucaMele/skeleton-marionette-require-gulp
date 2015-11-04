d2606154beb3b5a880407ab6cdd29a6df0a6fab9
Luca Mele <lucamele1@hotmail.com>
2015-11-04 20:30:29 +0100

    quick build example

--- README.md
+++ README.md
@@ -4,17 +4,26 @@ This is a skaleton for a SPA app with requireJs, BackboneJs, MarionetteJs and JS
 
 ##How to install:##
 
+
 First install all npm dependencies:
+
 `npm install`
 
 If you don't have already bower as a global node_module:
+
 `npm install -g bower`
 
 Install all bower dependencies:
+
 `bower install`
 
 Start everything with:
+
 `gulp`
+
+##A quick build example of require##
+
+You can execute `gulp build` to have a minified and bundled version of the whole project. To test it, run the script from index-built.html
 
 
 ##References and good to know:##
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp'),
     template = require('gulp-template-compile'),
     concat  = require('gulp-concat'),
-    clean = require('gulp-clean');
+    clean = require('gulp-clean'),
+    requirejsOptimize = require('gulp-requirejs-optimize'),
+    p = require('./package.json');
 
 // cleanup
 gulp.task('clean', function () {
@@ -9,13 +11,39 @@ gulp.task('clean', function () {
         .pipe(clean({force: true}));
 });
 
+// cleanup
+gulp.task('clean-build', function () {
+    return gulp.src('./dist/' + p.name + '_' + p.version + '.min.js', {read: false})
+        .pipe(clean({force: true}));
+});
+
 // build the templates
 gulp.task('jst', ['clean'], function () {
-    gulp.src('templates/**/*.html')
+    return gulp.src('templates/**/*.html')
         .pipe(template())
         .pipe(concat('templates.js'))
         .pipe(gulp.dest('./templates'));
 });
 
-gulp.task('default', ['jst']);
-gulp.watch('templates/**/*.html', ['jst']);
+// build
+gulp.task('build', ['clean-build', 'jst'], function () {
+    var config  = {
+        baseUrl: __dirname,
+        mainConfigFile: 'init.js',
+        generateSourceMaps: false,
+        out: '' + p.name + '_' + p.version + '.min.js',
+        optimize: 'uglify2',
+        findNestedDependencies: true,
+        // Include a minimal AMD implementation shim.
+        name: 'node_modules/almond/almond.js',
+        wrap: true,
+        preserveLicenseComments: true
+    };
+    return gulp.src('init.js')
+        .pipe(requirejsOptimize(config))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['jst'], function() {
+    gulp.watch('templates/**/*.html', ['jst']);
+});
